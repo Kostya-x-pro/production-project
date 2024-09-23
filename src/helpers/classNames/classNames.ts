9ed3a0c171e8d14c5b07bea6dabbbb3259f94b45
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-09-23 11:32:11 +0300

    classNames создаем git репозиторий

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,23 @@
+type Mods = Record<string, boolean | string>;
+
+// Параметры функции:
+// class - главный класс например .app
+// mode - объект с модами (как ключь идет название класса а как значение Boolean тип)
+// additional - массив дополнительных классов
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className, value]) => className)
+  ].join(" ");
+}
+
+classNames("remove-btn", { hoverd: true, selectebel: true, red: false }, [
+  "with_text",
+]);
