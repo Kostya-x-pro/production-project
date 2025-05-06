0db0f0486503050fbdecaca26dadeaf29e141c07
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-05-06 11:48:45 +0300

    add credentials for vercel

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,11 +2,15 @@ import { LOCAL_STORAGE_THEME_KEY } from 'app/providers/ThemeProvider/lib/ThemeCo
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
-const baseURL = __IS_DEV__ ? 'http://localhost:8000' : 'https://production.ru';
-
 export const $api = axios.create({
-    baseURL,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+    baseURL: __API__,
+    withCredentials: true,
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        const token = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+        config.headers.Authorization = token || '';
+    }
+    return config;
 });
